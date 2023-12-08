33503f8f09fd37bed2eb2c3cc9cfcfed6ddddefd
gab <givanes@pm.me>
2023-12-08 14:40:28 +0100

    sendmail

--- main.go
+++ main.go
@@ -2,28 +2,98 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
-	"github.com/karim-w/go-azure-communication-services/emails"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/karim-w/go-azure-communication-services/emails"
+	"github.com/microsoft/ApplicationInsights-Go/appinsights"
 )
 
+var products map[int]Product = map[int]Product{
+	1: {"Milk"},
+	2: {"Butter"},
+}
+
+type Product struct {
+	Name string
+}
+
+func init() {
+	flag.StringVar(&instrumentationKey, "instrumentationKey", os.Getenv("INSTRUMENTATION_KEY"), "set instrumentation key from azure portal")
+	telemetryClient = appinsights.NewTelemetryClient(instrumentationKey)
+	/*Set role instance name globally -- this is usually the name of the service submitting the telemetry*/
+	telemetryClient.Context().Tags.Cloud().SetRole("aifapi")
+	/*turn on diagnostics to help troubleshoot problems with telemetry submission. */
+	appinsights.NewDiagnosticsMessageListener(func(msg string) error {
+		log.Printf("[%s] %s\n", time.Now().Format(time.UnixDate), msg)
+		return nil
+	})
+}
+
+var (
+	telemetryClient    appinsights.TelemetryClient
+	instrumentationKey string
+)
 var host = "zagocbscdv01ew1cs01.france.communication.azure.com"                                      // os.Getenv("ACS_HOST")
 var key = "/On9q6QNWkbAg5KATebndcMy/IXuyO1qeTAEhMs3UfYtpgJVF2G9fEpLpbySTFbv0WnXzw5SfHgFPq6tSkeAAg==" // os.Getenv("ACS_KEY")
 // recipient := "givanes@pm.me"                                                                      // os.Getenv("EMAIL_RECIPIENT")
 var sender = "DoNotReply@d5f7e57b-6ade-4383-abac-2bf51413c4fd.azurecomm.net" // os.Getenv("EMAIL_SENDER")
 
-func sendEmail(w http.ResponseWriter, r *http.Request) {
-	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
+func getProducts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, products)
+}
 
-	subject := r.URL.Query().Get("subject")
-	body := r.URL.Query().Get("body")
-	recipient := r.URL.Query().Get("recipient")
-	name := r.URL.Query().Get("name")
+func getProduct(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.String(http.StatusBadRequest, "invalid product identifier")
+		return
+	}
+	p, ok := products[id]
+	if !ok {
+		c.String(http.StatusNotFound, "Product not found")
+		return
+	}
+	c.IndentedJSON(http.StatusOK, p)
 
-	if name != "" {
-		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
+}
+
+func get_port() string {
+	port := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		port = ":" + val
+	}
+	return port
+}
+
+func sendmail(c *gin.Context) {
+	fmt.Println("send email")
+	trace := appinsights.NewTraceTelemetry("begin create application", appinsights.Information)
+	telemetryClient.Track(trace)
+
+	// define custom type
+	type Input struct {
+		Subject   string `json:"subject" binding:"required"`
+		Body      string `json:"body" binding:"required"`
+		Recipient string `json:"recipient" binding:"required"`
+	}
+
+	var input Input
+
+	if err := c.ShouldBindBodyWith(&input, binding.JSON); err != nil {
+		trace := appinsights.NewTraceTelemetry("failed to bind request params to go struct for create application", appinsights.Critical)
+		telemetryClient.Track(trace)
+		telemetryClient.TrackException(err)
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return
 	}
 
 	client := emails.NewClient(host, key, nil)
@@ -34,13 +104,13 @@ func sendEmail(w http.ResponseWriter, r *http.Request) {
 		},
 		SenderAddress: sender,
 		Content: emails.Content{
-			Subject:   subject,
-			PlainText: body,
+			Subject:   input.Subject,
+			PlainText: input.Body,
 		},
 		Recipients: emails.Recipients{
 			To: []emails.ReplyTo{
 				{
-					Address: recipient,
+					Address: input.Recipient,
 				},
 			},
 		},
@@ -48,24 +118,29 @@ func sendEmail(w http.ResponseWriter, r *http.Request) {
 
 	result, err := client.SendEmail(context.TODO(), payload)
 	if err != nil {
-		fmt.Println(err)
-	}
-	if result.ID == "" {
-		fmt.Println("TrackingId is empty")
-	}
-	if result.Status == "" {
-		fmt.Println("Status is empty")
+		c.String(http.StatusNotFound, "email not send")
+		return
+	} else {
+		trace := appinsights.NewTraceTelemetry("email send", appinsights.Information)
+		telemetryClient.Track(trace)
 	}
 
-	fmt.Fprint(w, message)
+	c.IndentedJSON(http.StatusOK, result)
+}
+
+func sendmail2(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, products)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
 
 func main() {
-	listenAddr := ":8080"
-	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
-		listenAddr = ":" + val
-	}
-	http.HandleFunc("/sendemail", sendEmail)
-	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
-	log.Fatal(http.ListenAndServe(listenAddr, nil))
+	r := gin.Default()
+	r.GET("/api/products", getProducts)
+	r.GET("/api/products/:id", getProduct)
+	r.POST("/api/sendmail", sendmail)
+	r.GET("/api/sendmail", sendmail2)
+	r.Run(get_port())
 }
